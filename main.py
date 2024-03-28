cc1b90275f493836dd2a38e2986f5595c00716da
WeNny5 <abdelfatahwenny@yahoo.com>
2024-03-28 08:54:47 +0200

    added the rest of all functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def insert_letter(original_string, letter, position):
     return ''.join([original_string[:position], letter, original_string[position:]])
 
@@ -117,10 +120,112 @@ def elimination_universal(text):
         if final[i] == '∀' and str.islower(final[i + 1]):
             final = replace_letter(final, '', i)
             final = replace_letter(final, '', i)
+            i -= 1
         i += 1
         length = len(final)
     return final
 
 
-test = "∀y((∃x Test(y, x)) ∧ Easy(y) ⇒ ∀z Pass(z, y))"
-print(elimination_universal(prenex_form(test)))
+def skolmization(text):
+    final = text
+    q_para = []
+    q_quant_A = []
+    q_quant_E = []
+    i = 0
+    length = len(final)
+
+    while i < length:
+        if final[i] == '∀':
+            q_quant_A.append(final[i + 1])
+        elif final[i] == '∃':
+            q_quant_E.append(final[i + 1])
+            final = replace_letter(final, '', i)
+            final = replace_letter(final, '', i)
+            continue
+        elif final[i] == '(':
+            if len(q_quant_E) == 0:
+                q_quant_A = []
+            else:
+                q_para.append(final[i])
+        elif final[i] == ')' and len(q_para) != 0:
+            q_para.pop()
+        elif final[i] in q_quant_E and len(q_para) != 0:
+            if len(q_quant_A) != 0:
+                rd = "F{}".format(random.randrange(0, 100, 1))
+                co = "{}({})".format(rd, q_quant_A).replace('[', '').replace(']', '').replace('\'', '')
+                final = replace_letter(final, co, i)
+            else:
+                rd = ""
+                for j in range(4):
+                    rd = insert_letter(rd, random.choice(string.ascii_lowercase), len(rd) - 1)
+                rd = insert_letter(rd, random.choice(string.ascii_uppercase), 0)
+                final = replace_letter(final, rd, i)
+        i += 1
+    return final
+
+
+def distribute_or_over_and(text):
+    parts = text.split("∨")
+    if len(parts) == 1:
+        return [text]
+    else:
+        left = distribute_or_over_and(parts[0])
+        right = distribute_or_over_and("∨".join(parts[1:]))
+        return [l + r for l in left for r in right]
+
+
+def fol_to_conjunction(text):
+    text = remove_implication(text)
+    disjunctions = distribute_or_over_and(text)
+    return "∧".join(disjunctions)
+
+
+def divide_into_clauses(formula):
+    clauses = formula.split("∧")
+    return [clause.strip() for clause in clauses]
+
+
+def standardize(text):
+    i = 0
+    final = text
+    var = []
+    length = len(final)
+
+    while i < length:
+        if str.islower(final[i]):
+            if final[i] not in var:
+                var.append(final[i])
+            else:
+                rd = final[i]
+                while rd in var:
+                    rd = random.choice(string.ascii_lowercase)
+                str.replace(final, final[i], rd)
+        i += 1
+    return final
+
+
+def convert_to_CNF(text):
+    final = text
+    print("start: {}".format(final))
+    final = remove_implication(final)
+    print("after removing implication: {}".format(final))
+    final = de_morgan(final)
+    print("after DeMorgan: {}".format(final))
+    final = double_negation(final)
+    print("after removing double negation: {}".format(final))
+    # final = standardize(final)
+    # print("after standardization: {}")
+    final = prenex_form(final)
+    print("Moving quantifiers to the left: {}".format(final))
+    final = skolmization(final)
+    print("replacing there exist: {}".format(final))
+    final = elimination_universal(final)
+    print("eliminating universal quantifiers: {}".format(final))
+    final = fol_to_conjunction(final)
+    print("converting to conjunction form: {}".format(final))
+    final = divide_into_clauses(final)
+    print("clauses: {}".format(final))
+
+
+test = "∃x∀y∀z (((P(y)) → (Q(z))) → ((P(x)) → (Q(x))))"
+convert_to_CNF(test)
